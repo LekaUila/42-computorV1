b1e0e3412868d0e29666926e6170ff905d1d9310
Leka Uïla <liam.flandrinck.58@gmail.com>
2024-04-23 20:07:36 +0200

    adding opti number and number

--- class_d/operation.py
+++ class_d/operation.py
@@ -6,9 +6,28 @@
 #    By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/23 13:34:57 by lflandri          #+#    #+#              #
-#    Updated: 2024/04/23 16:32:20 by lflandri         ###   ########.fr        #
+#    Updated: 2024/04/23 20:07:05 by lflandri         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+def power(flt1, flt2) -> float:
+    result = flt1
+    if flt2 == 0 :
+        return 1
+    if flt2 == 1 :
+        return flt1
+    for i in range(int(flt2)):
+        result *= flt1
+    return result
+
+def sqrt(n):
+    i = 0
+    result = 1
+    while i < 50:
+        result = (result + (n / result)) / 2
+        i+=1
+    return result
+    
 
 def parseValueOperation(str):
     i = 0
@@ -78,9 +97,96 @@ class operation:
                 this.left = parseValueOperation(str)
         except BaseException as exeption :
             raise exeption
-                
-    def selfOpti(this):
-        return
+        
+    def opti(this):
+        nbModif = 42
+        while nbModif > 0:
+            nbModif = this.selfOptiNumberAndNumber()
+            
+    def selfOptiNumberAndOperation(this):
+        # TODO : finish function
+        opera = this
+        degre = 0
+        modifNb = 0
+        while opera != None :
+            result = None
+            if type(opera.left) == operation:
+                # print(f"Enter other branch {opera.left}")
+                modifNb += opera.left.selfOptiNumberAndOperation()
+                # print("Exit other branch")
+            elif (type(opera.left) == float and opera.right != None and type(opera.right.left) == operation) or (type(opera.left) == operation and opera.right != None and type(opera.right.left) == float):
+                # print(f"try for {opera.left} {opera.operator} {opera.right.left}")
+                if type(opera.left) == float:
+                    nb = opera.left
+                    ope = opera.right.left
+                else :
+                    nb = opera.right.left
+                    ope = opera.left
+                if opera.operator == "*" and degre < 2 and (opera.right.operator == None or opera.right.operator != "^"):
+                    result = opera.left * opera.right.left
+                # elif opera.operator == "/" and opera.right.left == 0.0 and degre < 2 and (opera.right.operator == None or opera.right.operator != "^"):
+                #     raise BaseException("can't divise by 0.")
+                # elif opera.operator == "/" and degre < 2 and (opera.right.operator == None or opera.right.operator != "^"):
+                #     result = opera.left / opera.right.left
+
+                # elif opera.operator == "^":
+                #     result = power(opera.left, opera.right.left)
+                if result != None :
+                    # print(f"opera done for {opera.left} {opera.operator} {opera.right.left}")
+                    opera.operator = opera.right.operator
+                    opera.right = opera.right.right
+                    opera.left = result
+                    modifNb += 1
+            if opera.operator != None and opera.operator == "^":
+                degre = 2
+            elif opera.operator != None and opera.operator in "/*":
+                degre = 1
+            else :
+                degre = 0
+        return modifNb
+
+        
+    def selfOptiNormalNumber(this):
+        opera = this
+        degre = 0
+        modifNb = 0
+        while opera != None :
+            result = None
+            if type(opera.left) == operation:
+                # print(f"Enter other branch {opera.left}")
+                modifNb += opera.left.selfOptiNumberAndNumber()
+                # print("Exit other branch")
+                if opera.left.operator == None:
+                    opera.left = opera.left.left
+            elif type(opera.left) == float and opera.right != None and type(opera.right.left) == float:
+                # print(f"try for {opera.left} {opera.operator} {opera.right.left}")
+                if opera.operator == "/" and opera.right.left == 0.0 and degre < 2 and (opera.right.operator == None or opera.right.operator != "^"):
+                    raise BaseException("can't divise by 0.")
+                elif opera.operator == "/" and degre < 2 and (opera.right.operator == None or opera.right.operator != "^"):
+                    result = opera.left / opera.right.left
+                elif opera.operator == "*" and degre < 2 and (opera.right.operator == None or opera.right.operator != "^"):
+                    result = opera.left * opera.right.left
+                elif opera.operator == "+" and degre < 1 and (opera.right.operator == None or opera.right.operator not in "*/^"):
+                    result = opera.left + opera.right.left
+                elif opera.operator == "-" and degre < 1 and (opera.right.operator == None or opera.right.operator not in "*/^"):
+                    result = opera.left - opera.right.left 
+                elif opera.operator == "^":
+                    result = power(opera.left, opera.right.left)
+                if result != None :
+                    # print(f"opera done for {opera.left} {opera.operator} {opera.right.left}")
+                    opera.operator = opera.right.operator
+                    opera.right = opera.right.right
+                    opera.left = result
+                    modifNb += 1
+            if opera.operator != None and opera.operator == "^":
+                degre = 2
+            elif opera.operator != None and opera.operator in "/*":
+                degre = 1
+            else :
+                degre = 0
+            if result == None :
+                opera = opera.right
+        return modifNb
     
     def print(this):
         opera = this
--- main.py
+++ main.py
@@ -6,63 +6,27 @@
 #    By: lflandri <liam.flandrinck.58@gmail.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/19 16:39:00 by lflandri          #+#    #+#              #
-#    Updated: 2024/04/23 16:13:59 by lflandri         ###   ########.fr        #
+#    Updated: 2024/04/23 18:49:01 by lflandri         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 from class_d.operation import operation
-# def convertNumber(str: str ) -> float:
-    
-#     return 
-
-# def createInstructionList(equation: str) -> list:
-#     instructionList = []
-#     actualInstruction = {"coef": None, "degree" : 0, "sign" : "1"}
-#     needSign = False
-#     needNumber = False
-#     needToBeReverse = False
-#     numberCreator = ""
-#     isExponent = False
-#     for i in range(len(equation)):
-#         if equation[i] != " ":
-#             if equation[i] in "+-":
-#                 instructionList.append(actualInstruction)
-#                 needSign = False
-#                 if equation[i] == "-" :
-#                     actualInstruction = {"coef": None, "degree" : 0, "sign" : "-1"}
-#                 else :
-#                     actualInstruction = {"coef": None, "degree" : 0, "sign" : "1"}
-#             elif equation[i] in "*/" :
-#                 needSign = False
-#                 if (equation[i] == "/") :
-#                     needToBeReverse = True
-#             elif equation[i] in "0123456789." :
-#                 numberCreator += equation[i]
-#             elif equation[i] == "x" :
-#                 actualInstruction["degree"] += 1
-#             elif equation[i] == "^" :
-#                 if isExponent :
-#                     print(f"Error : Can't add an other exponent at pos {i}.")
-#                     exit(1)
-#                 else :
-#                     isExponent = True
-#             else :
-#                 print(f"Error : unknow character at pos {i}.")
-#                 exit(1)
-    
-#     return instructionList
 
 def __main__() -> int:
     sys.setrecursionlimit(2000000000)
     try :
         if len(sys.argv) != 2:
             raise (BaseException("Wrong number of argument."))
-    
+        #TODO separer les 2 entré en temps que 2 partire de l'équation
         ope = operation(sys.argv[1])
+        ope.print()
+        ope.opti()
+        print("__________________________________________")
         ope.print()
     except BaseException as exeption :
         print(f"Error : {exeption.args[0]} ")
+        # o = 1/0
         return 1
     return 0
 
